9731a074cb39ba62fb920f29e777112e6ae10d52
Adriel Fernandes <adrielfernandes321@gmail.com>
2024-03-25 16:53:40 -0300

    feat: added icons to about section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,5 +3,6 @@ import "./slider.js"
 import "./animations.js"
 import "./socials.js"
 import "./exitModal.js"
+import "./mediaQueries.js"
 
 
---
+++ src/assets/js/mediaQueries.js
@@ -0,0 +1,39 @@
+document.addEventListener( 'DOMContentLoaded', function () { 
+  function changeAboutCardsSectionAnimation() {
+    const aboutCards = document.querySelectorAll(".about__float-cards")
+    if ($(window).width() <= 664) {
+      aboutCards.forEach(function(el, index) {
+        if(index % 2 === 0) {
+          $(el).removeClass("left")
+          if(!el.classList.contains("right")) {
+            $(el).addClass("right")
+          }
+        } else {
+          $(el).removeClass("right")
+          if(!el.classList.contains("left")) {
+            $(el).addClass("left")
+          }
+        }
+      })
+    } else {
+      aboutCards.forEach(function(el, index) {
+        const cardNumber = index + 1
+        console.log(cardNumber === 1 || cardNumber === 2)
+        if(cardNumber === 1 || cardNumber === 2) {
+          $(el).removeClass("right")
+          if(!el.classList.contains("left")) {
+            $(el).addClass("left")
+          }
+        } else {
+          $(el).removeClass("left")
+          if(!el.classList.contains("right")) {
+            $(el).addClass("right")
+          }
+        }
+      })
+    }
+  }
+  
+  addEventListener("resize", (_) => { changeAboutCardsSectionAnimation() });
+  changeAboutCardsSectionAnimation()
+})
