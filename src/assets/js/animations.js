59ccae06f13246291829d2973d08ce15ebe1218e
Adriel Fernandes <adrielfernandes321@gmail.com>
2024-04-05 20:03:00 -0300

    fix: added animated css node module

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -2,40 +2,40 @@ import { clipCarousel } from './slider.js'
 
 document.addEventListener( 'DOMContentLoaded', function () { 
 
-  const animatedEl = document.querySelectorAll(".animate__animated")
+  const animatedEl = document.querySelectorAll(".animated")
 
   let observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
         if (entry.target.classList.contains("title")) {
-          entry.target.classList.toggle("animate__fadeInDown", entry.isIntersecting)
+          entry.target.classList.toggle("fadeInDown", entry.isIntersecting)
         }
         if (entry.target.classList.contains("about-carousel")) {
-          entry.target.classList.toggle("animate__zoomIn", entry.isIntersecting)
+          entry.target.classList.toggle("zoomIn", entry.isIntersecting)
           if(entry.isIntersecting) { 
             clipCarousel.mount()
           }
         }
         if (entry.target.classList.contains("left")) {
-          entry.target.classList.toggle("animate__slideInLeft", entry.isIntersecting)
+          entry.target.classList.toggle("slideInLeft", entry.isIntersecting)
           entry.target.classList.toggle("show-arrow", entry.isIntersecting)
         }
         if (entry.target.classList.contains("right")) {
-          entry.target.classList.toggle("animate__slideInRight", entry.isIntersecting)
+          entry.target.classList.toggle("slideInRight", entry.isIntersecting)
           entry.target.classList.toggle("show-arrow", entry.isIntersecting)
         }
         if (entry.target.classList.contains("main-carousel")) {
-          entry.target.classList.toggle("animate__slideInLeft", entry.isIntersecting)
+          entry.target.classList.toggle("slideInLeft", entry.isIntersecting)
         }
         if (entry.target.classList.contains("thumbnail-wrapper")) {
-          entry.target.classList.toggle("animate__fadeIn", entry.isIntersecting)
+          entry.target.classList.toggle("fadeIn", entry.isIntersecting)
         }
         if (entry.target.classList.contains("secondary-carousel__image")) {
-          entry.target.classList.toggle("animate__slideInLeft",  entry.isIntersecting)
+          entry.target.classList.toggle("slideInLeft",  entry.isIntersecting)
           if(entry.isIntersecting) {
             setTimeout(function() {
               document.querySelectorAll('.secondary-carousel__image').forEach(function(el) {
-                $(el).removeClass("animate__animated")
+                $(el).removeClass("animated")
               })
               $('#thumbnail-carousel ').css('pointer-events', 'all')
               $('.thumbnail-wrapper ').css('cursor', 'initial')
@@ -43,7 +43,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
           }
         }
         if (entry.target.classList.contains("contact__card")) {
-          entry.target.classList.toggle("animate__zoomIn", entry.isIntersecting)
+          entry.target.classList.toggle("zoomIn", entry.isIntersecting)
         }
 
         if(entry.isIntersecting) observer.unobserve(entry.target)
