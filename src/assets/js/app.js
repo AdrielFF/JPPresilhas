328ca98f517d7f98237e0b9628d19d55d2808af4
Adriel Fernandes <adrielfernandes321@gmail.com>
2024-04-10 18:34:32 -0300

    removed unused bootstrap scripts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,3 +4,21 @@ import "./animations.js"
 import "./socials.js"
 import "./exitModal.js"
 import "./mediaQueries.js"
+
+import "../../../node_modules/bootstrap/js/dist/util/index.js"
+
+import "../../../node_modules/bootstrap/js/dist/dom/data.js"
+import "../../../node_modules/bootstrap/js/dist/dom/event-handler.js"
+import "../../../node_modules/bootstrap/js/dist/dom/selector-engine.js"
+import "../../../node_modules/bootstrap/js/dist/dom/manipulator.js"
+
+import "../../../node_modules/bootstrap/js/dist/util/config.js"
+import "../../../node_modules/bootstrap/js/dist/util/component-functions.js"
+import "../../../node_modules/bootstrap/js/dist/util/backdrop.js"
+import "../../../node_modules/bootstrap/js/dist/util/focustrap.js"
+import "../../../node_modules/bootstrap/js/dist/util/scrollbar.js"
+
+import "../../../node_modules/bootstrap/js/dist/base-component.js"
+import "../../../node_modules/bootstrap/js/dist/collapse.js"
+import "../../../node_modules/bootstrap/js/dist/modal.js"
+
